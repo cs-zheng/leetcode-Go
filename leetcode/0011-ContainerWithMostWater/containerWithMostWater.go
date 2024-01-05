bbd9e9e186c961367789d41b78cccf9e9e2eef01
local <admin@changsheng.dev>
2024-01-05 19:35:45 +0800

    初始化

---
+++ leetcode/0011-ContainerWithMostWater/containerWithMostWater.go
@@ -0,0 +1,9 @@
+package lc0011
+
+func CallFunc(height []int) int {
+	return maxArea(height)
+}
+
+func maxArea(height []int) int {
+	return 0
+}
---
+++ leetcode/main.go
@@ -0,0 +1,7 @@
+package main
+
+import lc0011 "leetcode-go/leetcode/0011-ContainerWithMostWater"
+
+func main() {
+	lc0011.CallFunc([]int{1, 2})
+}
